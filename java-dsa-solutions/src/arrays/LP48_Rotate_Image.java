d131de1373b15c9fa54527c71de8ce1421d105fe
MuraliM28 <muralim@kyyba.com>
2025-06-17 20:23:21 +0530

    Update LP48_Rotate_Image.java

--- java-dsa-solutions/src/arrays/LP48_Rotate_Image.java
+++ java-dsa-solutions/src/arrays/LP48_Rotate_Image.java
@@ -1,10 +1,25 @@
 package arrays;
 
+import java.util.Arrays;
+
+/*
+48. Rotate Image
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify 
+the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+*/
 public class LP48_Rotate_Image {
 
 	public static void main(String[] args) {
 		int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
 		rotateImage(matrix);
+		
+		for(int[] a : matrix) {
+			System.out.println(Arrays.toString(a));
+		}
 	}
 	public static void rotateImage(int[][] matrix) {
 		int m = matrix.length;
@@ -15,6 +30,23 @@ public class LP48_Rotate_Image {
 				matrix[i][j]=matrix[j][i];
 				matrix[j][i]=temp;
 			}
+		}
+		
+		for(int i=0; i<m; i++) {
+			reverseRow(matrix[i]);
+		}
+	}
+	
+	static void reverseRow(int[] arr) {
+		int left = 0; 
+		int right = arr.length-1;
+		
+		if(left<right) {
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+            right--;
 		}
 	}
 
